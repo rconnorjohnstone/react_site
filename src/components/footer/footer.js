17d352cb4755d06ef62f9d079cb3e3f8c105cee0
Connor <connor@richardconnorjohnstone.com>
2019-09-22 20:57:52 -0600

    mostly done with the landing page. need to add functionality

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,10 +8,10 @@ class Footer extends React.Component {
 
         <div className="footer_links">
 
-          <button><a href="https://www.github.com/rconnorjohnstone" ><p><i className="fa fa-github"></i></p></a></button>
-          <button><a href="https://www.instagram.com/rconnorjohnstone" ><p><i className="fa fa-instagram"></i></p></a></button>
-          <button><a href="https://www.linkedin.com/rconnorjohnstone" ><p><i className="fa fa-linkedin"></i></p></a></button>
-          <button><a href="mailto:connor@richardconnorjohnstone.com" ><p><i className="fa fa-envelope-open"></i></p></a></button>
+          <a href="https://www.github.com/rconnorjohnstone" ><p><i className="fa fa-github"></i></p></a>
+          <a href="https://www.instagram.com/r_connor_johnstone/" ><p><i className="fa fa-instagram"></i></p></a>
+          <a href="https://www.linkedin.com/in/rconnorjohnstone/" ><p><i className="fa fa-linkedin"></i></p></a>
+          <a href="mailto:connor@richardconnorjohnstone.com" ><p><i className="fa fa-envelope-open"></i></p></a>
 
         </div>
 
