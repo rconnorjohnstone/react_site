20c1d200ab1087214c4905a55f618a74147c3944
Connor <connor@richardconnorjohnstone.com>
2019-11-03 13:36:48 -0700

    Update

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './contact.css';
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    };
+  }
+
   render() {
     return (
       <div className="contact_wrapper">
@@ -10,17 +19,20 @@ class Contact extends React.Component {
 
         <div className="name_form " >
           <p> Name </p>
-          <input type="text" name="name" className="contact"/>
+          <input type="text" name="name" className="contact"
+                 onChange={e => this.setState({ name: e.target.value })}/>
         </div>
 
         <div className="email_form " >
           <p> Email </p>
-          <input type="text" name="email" className="contact" />
+          <input type="text" name="email" className="contact" 
+                 onChange={e => this.setState({ email: e.target.value })}/>
         </div>
 
         <div className="message_form " >
           <p> Message </p>
-          <textarea className="contact big_text" rows="6"></textarea>
+          <textarea className="contact big_text" rows="6"
+                    onChange={e => this.setState({ message: e.target.value })}></textarea>
         </div>
 
         <button className="round_button" > Send Message </button>
